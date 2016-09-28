8927c436729a2f66cfdaf00704f005351389c467
Brian Westerweel <brian@huizewesterweel.nl>
2016-09-28 19:16:48 +0200

    Made a start with exercise 2.1.2

--- Homework02/exercise 2-1.py
+++ Homework02/exercise 2-1.py
@@ -2,6 +2,7 @@
 from pylab import *
 from scipy.io import loadmat
 from scipy import stats
+import numpy as np
 
 W = loadmat('Data/wine.mat')['X']
 
@@ -148,54 +149,70 @@ W = loadmat('Data/wine.mat')['X']
 #title('Histogram quality score')
 #xlabel('0-10')
 #ylabel('occurences')
+#
+#Alcohol = W[:,10]
+#Acidity = W[:,2]
+#Density = W[:,8]
+#Rating = W[:,11]
+#tmpAlc = [],[]
+#tmpAci = [],[]
+#tmpDen = [],[]
+#
+#for i in range (0, len(Alcohol)):
+#    if(Alcohol[i] < 200 and Alcohol[i] > 0.5):
+#        tmpAlc[0].append(Alcohol[i])
+#        tmpAlc[1].append(Rating[i])
+#    if(Acidity[i] > 0 and Acidity[i] < 20):
+#        tmpAci[0].append(Acidity[i])
+#        tmpAci[1].append(Rating[i])
+#    if(Density[i] > 0.1 and Density[i] < 10):
+#        tmpDen[0].append(Density[i])
+#        tmpDen[1].append(Rating[i])
+#        
+#CAlc = stats.zscore(tmpAlc[:,0])
+#subplot(2, 1, 1)
+#boxplot(CAlc)
+#title('Boxplot cleaned Alcohol')
+#ylabel('% vol.')
+#
+#subplot(2, 1, 2)
+#hist(tmpAlc[:,0])
+#title('Histogram cleaned Alcohol')
+#xlabel('% vol.')
+#ylabel('occurences')
+#      
+#CAci = stats.zscore(tmpAci[:,0])
+#subplot(2, 1, 1)
+#boxplot(CAci)
+#title('Boxplot cleaned volatide acidity')
+#ylabel('g/dm3')
+#
+#subplot(2, 1, 2)
+#hist(tmpAci[:,0])
+#title('Histogram cleaned volatide acidity')
+#xlabel('g/dm3')
+#ylabel('occurences')
+#        
+#CDen = stats.zscore(tmpDen[:,0])
+#subplot(2, 1, 1)
+#boxplot(CDen)
+#title('Boxplot cleaned density')
+#ylabel('% vol.')
+#
+#subplot(2, 1, 2)
+#hist(tmpDen[:,0])
+#title('Histogram cleaned density')
+#xlabel('g/cm3')
+#ylabel('occurences')
 
-Alcohol = W[:,10]
-Acidity = W[:,2]
-Density = W[:,8]
-tmpAlc = []
-tmpAci = []
-tmpDen = []
+#scatter(W[:,0], W[:,11])
+#xlabel('Volatide acidity (g/dm3)')
+#ylabel('Quality score (0-10)')
+#
+#print(stats.pearsonr(W[:,0], W[:,11]))
 
-for i in range (0, len(Alcohol)):
-    if(Alcohol[i] < 200 and Alcohol[i] > 0.5):
-        tmpAlc.append(Alcohol[i])
-    if(Acidity[i] > 0 and Acidity[i] < 20):
-        tmpAci.append(Acidity[i])
-    if(Density[i] > 0.1 and Density[i] < 10):
-        tmpDen.append(Density[i])
-        
-CAlc = stats.zscore(tmpAlc)
-subplot(2, 1, 1)
-boxplot(CAlc)
-title('Boxplot cleaned Alcohol')
-ylabel('% vol.')
+scatter(tmpAci[0], tmpAci[1])
+xlabel('Volatile acidity (acetic) (g/dm3)')
+ylabel('Quality score (0-10)')
 
-subplot(2, 1, 2)
-hist(tmpAlc)
-title('Histogram cleaned Alcohol')
-xlabel('% vol.')
-ylabel('occurences')
-      
-CAci = stats.zscore(tmpAci)
-subplot(2, 1, 1)
-boxplot(CAci)
-title('Boxplot cleaned volatide acidity')
-ylabel('g/dm3')
-
-subplot(2, 1, 2)
-hist(tmpAci)
-title('Histogram cleaned volatide acidity')
-xlabel('g/dm3')
-ylabel('occurences')
-        
-CDen = stats.zscore(tmpDen)
-subplot(2, 1, 1)
-boxplot(CDen)
-title('Boxplot cleaned density')
-ylabel('% vol.')
-
-subplot(2, 1, 2)
-hist(tmpDen)
-title('Histogram cleaned density')
-xlabel('g/cm3')
-ylabel('occurences')
+print(stats.pearsonr(tmpAci[0], tmpAci[1]))
